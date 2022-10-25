9870c156f3d98f05bccdfcc19d0c5591b0f9c872
Read Langworthy <rlangworthy@gmail.com>
2022-10-25 12:20:29 -0700

    done

--- javascript/recursive_count.js
+++ javascript/recursive_count.js
@@ -1,5 +1,13 @@
 function recursiveCount(num = 0) {
   // type your code here
+
+  if (num < 10) {
+    console.log(num)
+    recursiveCount( num+1 )
+  }
+  
+
+
 }
 
 if (require.main === module) {
